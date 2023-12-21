0c2df22a3c33710af7a657f191d82b6b978f8d66
Domingos Dias <domingos.adj@gmail.com>
2023-12-21 14:07:37 -0300

    startapp auth

--- core/settings.py
+++ core/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -126,7 +127,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / 'static',
 ]
 
 print(os.path.join(BASE_DIR, 'finances/static'))
--- core/urls.py
+++ core/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('finances/', include('finances.urls')),
+    path('', include('auth.urls')),
 ]
