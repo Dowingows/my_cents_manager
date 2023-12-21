75b3da7ab1f149bede0dac5eb22751d400b78cee
Domingos Dias <domingos.adj@gmail.com>
2023-12-21 15:08:05 -0300

    SigninView

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,6 +7,10 @@ app_name = 'authentication'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('signin', views.signin, name='signin'),
+    path(
+        'signin',
+        views.SigninView.as_view(template_name='signin.html'),
+        name='signin',
+    ),
     path('signup', views.signup, name='signup'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,11 +1,16 @@
-from django.http import HttpResponse
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from django.urls import reverse_lazy
+
 
 def home(request):
     return render(request, 'home.html')
 
-def signin(request):
-    return render(request, 'signin.html')
+
+class SigninView(LoginView):
+    template_name = 'signin.html'
+    next_page = reverse_lazy('authentication:home')
+
 
 def signup(request):
     return render(request, 'signup.html')
