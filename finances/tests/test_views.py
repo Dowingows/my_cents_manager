a32b3831b27d099078fd5791d2f0cb10397d716c
Domingos Dias <domingos.adj@gmail.com>
2023-12-22 16:57:14 -0300

    changes

--- finances/tests/test_views.py
+++ finances/tests/test_views.py
@@ -47,3 +47,39 @@ class ExpenseDetailViewTest(AuthenticationMixin, TestCase):
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
+
+
+class ExpenseCreateViewTest(AuthenticationMixin, TestCase):
+    def test_expense_create_view_not_authenticated(self):
+
+        url = reverse('finances:expense_create')
+
+        self.assertRequiresAuthentication(url)
+
+    def test_expense_create_view_authenticated(self):
+
+        self.authenticate_user()
+
+        response = self.client.get(reverse('finances:expense_create'))
+        self.assertEqual(response.status_code, 200)
+
+        form_data = {
+            'name': 'Test Expense',
+            'amount': 100.00,
+            'payment_date': '2023-12-15',
+            'due_date': '2023-12-20',
+            'user_id': self.test_user.pk,
+        }
+
+        response = self.client.post(
+            reverse('finances:expense_create'), data=form_data
+        )
+
+        self.assertRedirects(response, reverse('finances:expense_index'))
+
+        self.assertTrue(Expense.objects.filter(name='Test Expense').exists())
+
+        expense = Expense.objects.get(name='Test Expense')
+        self.assertEqual(expense.amount, 100.00)
+        self.assertEqual(str(expense.payment_date), '2023-12-15')
+        self.assertEqual(str(expense.due_date), '2023-12-20')
