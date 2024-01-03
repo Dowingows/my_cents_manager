5d0be9c43f9dab90861c4a1cbdbfb2ed035341e6
Domingos Dias <domingos.adj@gmail.com>
2024-01-03 15:07:38 -0300

    changes

--- core/settings.py
+++ core/settings.py
@@ -29,9 +29,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default="localhost 127.0.0.1 [::1]").split(" ")
 
-CSRF_TRUSTED_ORIGINS = [
-    os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
-]
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
