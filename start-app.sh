d93ce5146fedb627e21724eca3d293e1777585d7
Domingos Dias <domingos.adj@gmail.com>
2024-01-08 10:54:21 -0300

    changes

--- start-app.sh
+++ start-app.sh
@@ -7,5 +7,5 @@ docker compose up -d --build app
 CADDY_RUNNING=$(docker ps --format '{{.Names}}' | grep -E '^[^-]+-caddy-[0-9]+$')
 
 if [ -z "$CADDY_RUNNING" ]; then
-  docker compose up -d --build caddy
+  docker compose up -d caddy
 fi
